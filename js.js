6b72f89dee20c5bb11e81b001c28d9176e769e47
Jeremy Roelfs <jeremyroelfs@icloud.com>
2015-11-21 14:16:03 -0700

    backup

--- js.js
+++ js.js
@@ -1,35 +1,175 @@
 window.onload = function(){ //program contianer function
 
-jQeffects ={
+
+// jQeffects ={
 
     /* function : arguments */
       //'fade' : [''];
 
-}
+// }
 
 
+// $( '.button' ).click(function() {
+//   console.log("in function");
+//   var selectedEffect = $('#effectTypes').val();
+//   console.log("EFFECT: ", selectedEffect);
+//   $( '.effect' ).effect( selectedEffect, callback );
+
+// });
+
+// function callback() {
+//       setTimeout(function() {
+//         $( ".effect" ).removeAttr( "style" ).hide().fadeIn();
+//       }, 1000 );
+//     };
 
 
-$( '.button' ).click(function() {
-  console.log("in function");
-  var selectedEffect = $('#effectTypes').val();
-  console.log("EFFECT: ", selectedEffect);
-  $( '.effect' ).effect( selectedEffect, callback );
+// function newfunc( {
+//   sub = a*b
+
+//   return
+// }
+
+
+//BATTLE SYSTEM
+/*Needed
+    -Random seed generator
+    -State system
+    -Basic turns
+    -"Player" object container for tracking changes and game play
+    -"AI" object container for tracking changes and game play
+    -Card generator
+    -Deck container for play - for both players
+
+
+    *BATTLE VARS
+    - MANA, HEAL, EFFECT, ATTACK, WEAKNESS, STRENGTH
+
+    *ATTACK
+      - Fire
+      - Earth
+      - Water
+      - Solar
+      - Nature
+*/
+
+  //DEFINE VARIABLES
+  var turn = true;
+  var player = {
+      'hp' : 21
+  };
+  var enemy ={
+      'hp' : 21
+  };
+  var turn;
+
+
+  //Setup game based on random seed
+  function setup() {
+
+        update();
+
+        var rnd = randomNumGen(1,1000);//generate starter
+        if(rnd%2===0)
+        {
+          turn = false;
+          attack();
+        } //else we wait for player to click
+        
+   };
+
+
+   //Game Go
+   function initGame(){
+      //Fill out stuff
+      update();
+      
+
+      //User clicks or computer goes
+      if(turn){ //true is player, false is computer
+          console.log('Player attacks');
+          //player.hp -= damage;
+
+      }else{
+          console.log('Enemy attacks');
+          attack();
+      }
+   };
+
+
+   //Actual attack
+   function attack(){
+      var damage = randomNumGen(3,1);
+
+      //Add weaknesses and strengths here
+
+
+      return damage;
+   };
+
+
+   //UPDATE ALL
+   function update(){
+  
+      //Health
+      $('#playerHP').text(player.hp);
+      $('#enemyHP').text(enemy.hp);
+
+      //Stats
+
+
+      //Program
+      if(turn){
+        //Turn is true
+        $('#turn').text('Player');
+      }else{
+        //Turn is false
+        $('#turn').text('Computer');
+      }
+
+   };
+
+
+//------------CLICK FUNCTIONS---------------
+$('#attack').on('click', function(){
+    console.log('Player clicked attack');
+    var player;
+    var effects;
+    if(turn){
+        attack(player);
+
+    }
+
 
 });
 
-function callback() {
-      setTimeout(function() {
-        $( ".effect" ).removeAttr( "style" ).hide().fadeIn();
-      }, 1000 );
-    };
 
 
-function newfunc( {
-  sub = a*b
+//-----------HELPER FUNCTIONS---------------
 
-  return
-}
+//Random Number Generator
+  function randomNumGen(min,max){
+      return Math.floor(Math.random() * (max - min) + min);
+  };
+
+
+
+
+
+setup();
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }(); /* END OF MAIN FUNCTION */
--- readme.md
+++ readme.md
@@ -1,20 +1,40 @@
 # Galvanize Individual Project Proposal
 
 ## Project Description
+Title: Karta 
+- Turn-based card playing game
 
 
 ## Who uses it?
+- Fun for all ages. The main purpose will be for entertainment.
 
 
 ## What outputs do they need?
+- Outputs will be points that determine the winner
 
 
 ## What inputs are needed to generate those outputs?
+- Basic state system
+- Basic AI system ... even a very basic seed generator to produce a number for the AI's decision
+- APIs:
+	- Images for cards
+	- Possibly Stats
 
 
 ## What technologies do you plan to use?
+- CSS/HTML
+- JQuery
+- JQuery UI
+- Javascript
+- Wikia API - characters/stats
+	- Possibly other APIs
+- Paint program (GIMP)
 
 
 ## Feature list
-
+Core)
+	-> Battle System - turn style point system
+		* 3rd system
+		* Winner based on 21 points. Win by 2.
+		* 
 
